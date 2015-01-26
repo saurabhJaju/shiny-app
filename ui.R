3653bdcb42f42bb680c042005444056f4163c9e3
saurabhJaju <saurabh.jaju2@gmail.com>
2015-01-26 05:58:50 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -15,12 +15,10 @@ shinyUI(pageWithSidebar(
                 h4('You entered'),
                 verbatimTextOutput("oTCP"),
                 h4('Which resulted in prediction of'),
-                verbatimTextOutput("prediction")
+                h4('Description'),
+                p("In the app a function calculates the percentage of data used for skype or surfing(input by the user). And a prediction is made as to what kind of data plan the user should buy the next time."),
+                p("i.e if the user consumes more data by using skype he should buy a data(internet) plan which has less jitter.(i.e a high speed plan) and if the user consumes more data by surfing than skype then he/she must by high total data plan. So the app basically recomends what kind of internet plan the user must buy based on his data usage pattern")
                 )
         )
 )
 
-##checkboxGroupInput("id2","Checkbox",
-##c("Value 1" = "1",
-  ##"Value 2" = "2",
-  ##"Value 3" = "3")),
